efab440bfea64da4bc359fe3802ff220af2a9101
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-16 05:39:51 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdarg.h>
+
+
+/**
+ * _printf - home made printf
+ *
+ * @format: array of things to be printed
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int count;
+
+	func_printer printers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent}
+	};
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(ap, format);
+
+	counter = prf_looper(format, printers, ap);
+
+	va_end(ap);
+
+	return (count);
+}
+
--- printf.h
+++ printf.h
@@ -4,18 +4,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-// helpers
+/* helpers */
 void _putchar(char c);
 void _puts(char *str);
 int _atoi(char *s);
 
-// printers
+/* printers */
 int print_char(va_list arg);
 int print_string(va_list arg);
-int print_percent (va_list arg);
+int print_percent(va_list arg);
 
-// struct
+/* struct */
 
+/**
+ * struct func_holder - structor for functions
+ *
+ * @symb: characters to the specifier
+ * @print_func: pointer to printer functions
+ *
+ */
 struct func_holder
 {
 	char *symb;
@@ -24,9 +31,9 @@ struct func_holder
 
 typedef struct func_holder func_printer;
 
-// main files
+/* main files */
 int _printf(const char *format, ...);
-int prf_looper(const char *format ,func_printer printers[] , va_list ap);
+int prf_looper(const char *format, func_printer printers[], va_list ap);
 
 
 
