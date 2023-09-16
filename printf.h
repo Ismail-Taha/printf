678b601941b28b9f2594820aec1540fd395dab7b
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-16 05:29:37 -0700

    header

---
+++ printf.h
@@ -0,0 +1,38 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+// helpers
+void _putchar(char c);
+void _puts(char *str);
+int _atoi(char *s);
+
+// printers
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent (va_list arg);
+
+// struct
+
+struct func_holder
+{
+	char *symb;
+	int (*print_func)(va_list arg);
+};
+
+typedef struct func_holder func_printer;
+
+// main files
+int _printf(const char *format, ...);
+int prf_looper(const char *format ,func_printer printers[] , va_list ap);
+
+
+
+
+
+
+
+#endif /* PRINTF_H */
+
