6ab4220c54a88272d8de2c8e0fc5b38389d07fe0
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-17 06:39:38 -0700

    gg

---
+++ switch_base.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * base_lane - calculate base length
+ *
+ * @n: number to be devised
+ * @dev: deviser
+ *
+ * Return: lenght
+ */
+
+
+int base_len(int n, int dev)
+{
+	int len;
+
+	len = 0;
+
+	while (n / dev > 0)
+	{
+		n = n / dev;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * int_to_binary - switch int to binary
+ *
+ * @n: integer to be switched
+ *
+ * Return: binary forme of n
+ */
+
+char *int_to_binary(int n)
+{
+	char *bin;
+	int len;
+
+	len = base_len(n, 2);
+	bin = malloc(sizeof(char) * (len + 1));
+	if (bin == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		bin[i] = '0' + (n % 2);
+		n = n / 2;
+	}
+	bin[len] = '\0';
+
+	return (bin);
+}
