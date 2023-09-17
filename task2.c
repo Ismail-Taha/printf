4cefd61aa3319868e8ded6a52eea9b6dae2cbc72
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-17 06:58:57 -0700

    task2

--- task2.c
+++ task2.c
@@ -1,15 +1,24 @@
 #include "main.h"
+/**
+ * pr_binary - print binary of an integer
+ * @args: list of arguments
+ * Return: number of characters printed
+ */
 
 
-
-int pr_binary()
+int pr_binary(va_list args)
 {
+	int i = 0;
+	unsigned int n;
+	char *bin;
+	int len;
+
+	n = va_arg(args, unsigned int);
 	
+	bin = int_to_binary(n);
 
+	len = _puts(bin);
+	free(bin);
 
-
-
-
-
-
+	return (len);
 }
