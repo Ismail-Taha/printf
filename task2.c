f84eb684c4cfd24f8e3c5d2c5f1599815392f1cd
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-17 05:26:08 -0700

    task1

--- task2.c
+++ task2.c
@@ -12,7 +12,9 @@ int pr_unsigned_int(va_list args)
 	int len;
 
 	n = va_arg(args, unsigned int);
-	len = pr_unsigned_num(n);
-
-	return (len);
+	if (n == 0)
+		return(pr_unsigned_num(n));
+	if (n < 0)
+		return (-1);
+	return(pr_unsigned_num(n));
 }
