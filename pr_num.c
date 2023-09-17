54e1305eee221be6aa43b86768cfee8cf3ee35ff
joe-0009 <youssefrachidi1994@gmail.com>
2023-09-17 04:36:34 -0700

    pr int

--- pr_num.c
+++ pr_num.c
@@ -2,10 +2,10 @@
 
 /**
  * pr_num - prints a number send to this function
- * @args: List of arguments
+ * @n: number to be printed
  * Return: The number of arguments printed
  */
-int pr_num(va_list args)
+int pr_num(int n)
 {
 	int n;
 	int len;
@@ -35,6 +35,35 @@ int pr_num(va_list args)
 		num = num % dev;
 		dev /=  10;
 		len++;
+	}
+	return (len);
+}
+
+/**
+ * pr_unsgined_num - Prints an unsigned number
+ * @n: unsigned integer to be printed
+ * Return: The amount of numbers printed
+ */
+int pr_unsgined_num(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	while (num / div > 9)
+		div *= 10;
+
+	while (div != 0)
+	{
+		len++;
+		_putchar('0' + num / div);
+		num %= div;
+		div /= 10;
 	}
 	return (len);
 }
